eb2801e10957e299502ea3555232f6d62db24da3
Don Irwin <don@falconwebtech.com>
2016-05-17 21:52:35 -0700

    styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,56 @@ import { Survey } from './survey/survey';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <h2>Surveys</h2>
-        <ul>
-        <li *ngFor="let survey of surveys"
-            [class.selected]="survey === selectedHero"
-            (click)="onSelect(survey)">
-            {{survey.Key}}
-        </li>
-        </ul>`,
+    templateUrl: 'app/app.component.html',    
+    styles:[`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .surveys {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .surveys li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: 0.3em 0 0 10px;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .surveys li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .surveys li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .surveys .text {
+            position: relative;
+            top: -3px;
+        }
+        .surveys .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+    `],
     providers: [SurveyService]
 })
 export class AppComponent { 
